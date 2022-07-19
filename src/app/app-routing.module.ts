31ce2049565d31c9105de0b6d61d2b78921ebc2d
“bhushan” <bhushan_ag@goolrant.com>
2022-07-19 21:58:49 +0530

    add services pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CloudServicesComponent } from './glrant-services/cloud-services/cloud-services.component';
+import { WebDevelopmentComponent } from './glrant-services/web-development/web-development.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -16,6 +18,14 @@ const routes: Routes = [
   {
       path: 'contact-us',
       component: ContactUsComponent,
+  },
+  {
+      path: 'cloud-service',
+      component: CloudServicesComponent,
+  },
+  {
+      path: 'web-development',
+      component: WebDevelopmentComponent,
   },
 ];
 
