f1ccabceae1d9ba3cdcf5803e7d15924b242021b
“bhushan” <bhushan_ag@goolrant.com>
2022-08-15 15:40:46 +0530

    complete home page mobile view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: "app-home",
@@ -7,9 +8,36 @@ import { Router } from "@angular/router";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  isMobile: boolean = false;
+  cellCount = 3;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    public breakpointObserver: BreakpointObserver
+    ) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+    .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.HandsetLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.Medium,
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape
+    ])
+    .subscribe((state: BreakpointState) => {
+        if (state.breakpoints[Breakpoints.XSmall] || state.breakpoints[Breakpoints.Small]) {
+            this.isMobile = true;
+            this.cellCount = 1;
+            console.log( 'Matches XSmall viewport');
+        } else {
+            this.isMobile = false;
+            this.cellCount = 3;
+        }
+    });
+  }
 
   contact() {
     this.router.navigate(["/contact-us"]);
